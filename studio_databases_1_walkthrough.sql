c0ef3871c9c87ad679531b78535b760176f66997
np303 <2ep303@gmail.com>
2019-04-15 14:30:06 -0500

    walkthrough

---
+++ studio_databases_1_walkthrough.sql
@@ -0,0 +1,26 @@
+--Crete a database called "movie-buff" with 2 tables in it, 'movies' 
+--and 'directors'
+CREATE TABLE movies(
+    movie_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(120),
+    year INTEGER,
+    director_id INTEGER(120)
+);
+
+--Creat a table for directors
+CREATE TABLE directors(
+    director_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first VARCHAR(120),
+    last VARCHAR(120),
+    country VARCHAR(120)
+);
+
+DROP TABLE movies;
+
+CREATE TABLE movies(
+    movie_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(120),
+    year INTEGER,
+    diretor_ID INTEGER,
+    FOREIGN KEY (director_id) REFERENCES directors(director_id)
+)
